6041aa3ad8223ced8e5463f94c5ccedb043f9add
muhammadfasihhashmi <muhammadfasihhashmii@gmail.com>
2025-12-28 17:31:06 +0500

    jndjnsjfnsdjkfbjk

---
+++ function.js
@@ -0,0 +1,16 @@
+// function add(name = "ahmad", age = 23) {
+//   console.log(name);
+//   console.log(age);
+//   console.log("shdj");
+// }
+// add();
+// add("fasih", 23);
+
+function sum(a, b, ...rest) {
+  console.log(rest);
+
+  return a + b;
+}
+let abs = sum(23, 23, 34, "hgdjj", 34);
+
+console.log(abs);
